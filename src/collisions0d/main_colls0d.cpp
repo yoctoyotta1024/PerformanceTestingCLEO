0c0b7b11ba9d3406f7cd2d1aed971b77c411ebe1
clara.bayley <clara.bayley@mpimet.mpg.de>
2024-12-12 10:51:24 +0100

    refactor: config kokkos correctly and print

--- src/collisions0d/main_colls0d.cpp
+++ src/collisions0d/main_colls0d.cpp
@@ -142,7 +142,8 @@ int main(int argc, char *argv[]) {
   }
 
   /* Initialise Kokkos parallel environment */
-  Kokkos::initialize(argc, argv);
+  Kokkos::initialize(Kokkos::InitializationSettings()
+                            .set_print_configuration(true));
   {
     /* Read input parameters from configuration file(s) */
     const std::filesystem::path config_filename(argv[1]);

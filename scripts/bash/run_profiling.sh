390eb2aa15e14a05e338c60efd5c406ae19c1ff2
clara.bayley <clara.bayley@mpimet.mpg.de>
2024-12-13 16:52:21 +0100

    fix: correct usage by default and in notes

--- scripts/bash/run_profiling.sh
+++ scripts/bash/run_profiling.sh
@@ -21,7 +21,7 @@ spack unload --all
 python=${1:-/work/bm1183/m300950/bin/envs/perftests/bin/python}
 path2src=${2:-/home/m/m300950/performance_testing_cleo}       # performance_testing_cleo root dir
 path2builds=${3:-${path2src}/builds}                          # builds in path2builds/[build_type]
-executable=${4:-collisions0d/colls0d}
+executable=${4:-colls0d}
 profilers=${5:-kerneltimer.spacetimestack}                    # list with '.' seperating profilers
 sbatch=${6:-sbatch}                                           # "sbatch" or otherwise false
 buildtypes=("${@:7}")                                         # "serial", "openmp" , "cuda" and/or "threads"

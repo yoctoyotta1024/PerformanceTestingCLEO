3a679cd1332e077650c01d912b70a8560f56fbf7
clara.bayley <clara.bayley@mpimet.mpg.de>
2024-12-19 09:49:53 +0100

    feat: new file to plot strong scaling

---
+++ scripts/plotting/strong_scaling.py
@@ -0,0 +1,20 @@
+"""
+Copyright (c) 2024 MPI-M, Clara Bayley
+
+-----  PerformanceTestingCLEO -----
+File: strong_scaling.py
+Project: plotting
+Created Date: Monday 9th December 2024
+Author: Clara Bayley (CB)
+Additional Contributors:
+-----
+Last Modified: Monday 9th December 2024
+Modified By: CB
+-----
+License: BSD 3-Clause "New" or "Revised" License
+https://opensource.org/licenses/BSD-3-Clause
+-----
+File Description:
+Script for plotting strong scaling results (increasing reesources for fixed problem size)
+from zarr xarray datasets for kokkos profiling data. Note: standard data format assumed.
+"""

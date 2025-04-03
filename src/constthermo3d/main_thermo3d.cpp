719e8181c671ba84526c4cf74764413ff13adc87
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-04-03 14:52:07 +0200

    refactor: add coord2 observer to thermo3d

--- src/constthermo3d/main_thermo3d.cpp
+++ src/constthermo3d/main_thermo3d.cpp
@@ -135,9 +135,10 @@ inline Observer auto create_superdrops_observer(const unsigned int interval,
   CollectDataForDataset<Store> auto msol = CollectMsol(dataset, maxchunk);
   CollectDataForDataset<Store> auto coord3 = CollectCoord3(dataset, maxchunk);
   CollectDataForDataset<Store> auto coord1 = CollectCoord1(dataset, maxchunk);
+  CollectDataForDataset<Store> auto coord2 = CollectCoord2(dataset, maxchunk);
 
   const auto collect_sddata =
-      coord1 >> coord3 >> msol >> radius >> xi >> sdgbxindex >> sdid;
+      coord2 >> coord1 >> coord3 >> msol >> radius >> xi >> sdgbxindex >> sdid;
   return SuperdropsObserver(interval, dataset, maxchunk, collect_sddata);
 }
 
